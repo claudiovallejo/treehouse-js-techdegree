203ea2140b959c8a3df83d2e2394e9294604b50d
Claudio Vallejo <hola@claudiovallejo.mx>
2016-04-26 17:09:20 -0500

    Started commenting script.js. Also, I commented out the currentQuote references inside the getRandomQuote().

--- Project 1/Development/js/script.js
+++ Project 1/Development/js/script.js
@@ -1,11 +1,15 @@
 /*  Quote Object - - - - - - - - - */
-function Quote(author, ids, quote, imagePath, imageSource, imageUrl) {
-  this.author = author;
-  this.ids = ids;
+function Quote(source, tags, quote, imagePath, imageSource, imageUrl, citation, date) {
+  //  The source parameter contains the Author's Name
+  this.source = source;
+  //  The tags parameter contains the Author's Specialties (for example 'Theoretical Physicist')
+  this.tags = tags;
   this.quote = quote;
   this.imagePath = imagePath;
   this.imageSource = imageSource;
   this.imageUrl = imageUrl;
+  this.citation = citation;
+  this.date = date;
 
 }
 
@@ -17,7 +21,9 @@ var quote1 = new Quote(
   "\"And those who were dancing were thought to be insane by those who couldn't hear the music.\"",
   "images/friedrich@2x.jpg",
   "Art Creation Forever",
-  "http://artcreationforever.com/images/friedrich-nietzsche/friedrich-nietzsche-06.jpg"
+  "http://artcreationforever.com/images/friedrich-nietzsche/friedrich-nietzsche-06.jpg",
+  "",
+  ""
 
 );
 
@@ -28,7 +34,9 @@ var quote2 = new Quote(
   "\"The two most important days in your life are the day you were born and the day you find out why.\"",
   "images/mark@2x.jpg",
   "Time Inc.",
-  "http://img2.timeinc.net/people/i/2015/news/150518/mark-twain-768.jpg"
+  "http://img2.timeinc.net/people/i/2015/news/150518/mark-twain-768.jpg",
+  "",
+  ""
 
 );
 
@@ -39,7 +47,9 @@ var quote3 = new Quote(
   "\"Everything that irritates us about others can lead us to an understanding of ourselves.\"",
   "images/carl@2x.jpg",
   "Biography.com",
-  " http://a2.files.biography.com/image/upload/c_fit,cs_srgb,dpr_1.0,h_1200,q_80,w_1200/MTE5NTU2MzE2MjY4NjkzMDAz.jpg"
+  " http://a2.files.biography.com/image/upload/c_fit,cs_srgb,dpr_1.0,h_1200,q_80,w_1200/MTE5NTU2MzE2MjY4NjkzMDAz.jpg",
+  "",
+  ""
 
 );
 
@@ -50,7 +60,9 @@ var quote4 = new Quote(
   "\"The greatest happiness is to know the source of unhappiness.\"",
   "images/fyodor@2x.jpg",
   "Populere Debiyat",
-  "http://www.populeredebiyat.com/wp-content/uploads/2012/01/dostoyevsky.jpg"
+  "http://www.populeredebiyat.com/wp-content/uploads/2012/01/dostoyevsky.jpg",
+  "",
+  ""
 
 );
 
@@ -61,7 +73,9 @@ var quote5 = new Quote(
   "\"A person who never made a mistake never tried anything new.\"",
   "images/albert@2x.jpg",
   "Philosophical Library",
-  "http://www.philosophicallibrary.com/wp-content/uploads/2016/03/Albert-Einstein-HD.jpg"
+  "http://www.philosophicallibrary.com/wp-content/uploads/2016/03/Albert-Einstein-HD.jpg",
+  "",
+  ""
 
 );
 //  Salvador Dalí
@@ -71,7 +85,9 @@ var quote6 = new Quote(
   "\"Intelligence without ambition is a bird without winds.\"",
   "images/salvador@2x.jpg",
   "Ecestaticos.com",
-  "http://www.ecestaticos.com/image/clipping/939/967b7be01cf74878acd2e25afd61d715/el-pintor-salvador-dali-en-1942-gtres.jpg"
+  "http://www.ecestaticos.com/image/clipping/939/967b7be01cf74878acd2e25afd61d715/el-pintor-salvador-dali-en-1942-gtres.jpg",
+  "",
+  ""
 
 );
 
@@ -82,7 +98,9 @@ var quote7 = new Quote(
   "\"Strength does not come from physical capacity. It comes from an indomitable will.\"",
   "images/mahatma@2x.jpg",
   "Keep It Worthy",
-  "http://keepitworthy.com/wp-content/uploads/2015/11/url.jpg"
+  "http://keepitworthy.com/wp-content/uploads/2015/11/url.jpg",
+  "",
+  ""
 
 );
 
@@ -93,7 +111,9 @@ var quote8 = new Quote(
   "\"The ultimate measure of a man is not where he stands in moments of comfort and convenience, but where he stands at times of challenge and controversy.\"",
   "images/martin@2x.jpg",
   "National Service",
-  "http://www.nationalservice.gov/sites/default/files/upload/mlklarge4.jpg"
+  "http://www.nationalservice.gov/sites/default/files/upload/mlklarge4.jpg",
+  "",
+  ""
 
 );
 
@@ -104,7 +124,9 @@ var quote9 = new Quote(
   "\"If you want to find the secrets of the universe, think in terms of energy, frequency and vibration.\"",
   "images/nikola@2x.jpg",
   "Widimedia.org",
-  "https://upload.wikimedia.org/wikipedia/commons/7/79/Tesla_circa_1890.jpeg"
+  "https://upload.wikimedia.org/wikipedia/commons/7/79/Tesla_circa_1890.jpeg",
+  "",
+  ""
 
 );
 
@@ -116,12 +138,15 @@ var quote10 = new Quote(
   "\"Every man's life ends the same way, it is only the details of how he lived and how he died that distinguish one man from another.\"",
   "images/ernest@2x.jpg",
   "The Pursuit of Sassiness",
-  "http://www.thepursuitofsassiness.com/wp-content/uploads/2012/12/hway8.jpg"
+  "http://www.thepursuitofsassiness.com/wp-content/uploads/2012/12/hway8.jpg",
+  "",
+  ""
 
 );
 
 /*  Quote Object Array - - - - - - - - - */
 var quotes = [quote1, quote2, quote3, quote4, quote5, quote6, quote7, quote8, quote9, quote10];
+//  Array that contains the quotes that have been randomly selected by the getRandomQuote()
 var quotesClone = [];
 var currentQuote = [];
 
@@ -133,25 +158,30 @@ var testProfiles = [];
 /*  Test Function - - - - - - - - - - */
 /*  This function runs everytime getRandomQuote() fetches a quote from the quotes array.
     This function stores the selected random quote into an array and, after all the quotes
-    have been selected, prints out the author names of the selected quotes.
-    I did this to make ensure quotes were not being repeated.
+    have been selected, prints out the author(aka source) names of the selected quotes.
+    I did this to ensure quotes were not being repeated.
 */
-
 function test(profile) {
   testProfiles.push(profile);
 
+  //  Prints a list (in order of selection) of the Author Names of the Quote Profiles
   if (testProfiles.length === 10) {
     var printList = "You have viewed the following profiles: ";
     console.log("Test Array has " + testProfiles.length + " profiles.");
 
+    //  Adds each Profile to the String
     for (var index = 0; index < testProfiles.length; index++) {
-      printList += (index + 1) + ". " + testProfiles[index].author + "; ";
+      printList += (index + 1) + ". " + testProfiles[index].source + "; ";
 
     }
 
+    //  After adding each Profile selected to the printList String, log the String to the console and clear the testProfile array.
     console.log(printList);
     testProfiles = [];
 
+  //  This code logs a String detailing how many Quote Objects the testProfiles array has, just to know how many quotes we've yet to view.
+  //  This block of code contains an interior if/else statement which simply makes sure the tense of the word 'profile' is correct.
+  //  For example, "Test Array has 1 profile." vs. "Test Array has 7 profiles."
   } else {
     if (testProfiles.length === 1) {
       console.log("Test Array has " + testProfiles.length + " profile.");
@@ -166,6 +196,9 @@ function test(profile) {
 }
 
 /*  Random Quote Function - - - - - - - - - - */
+/*  The Function responsible for selecting a random Quote from the quotes array AND pushing the selected quote to the quotesClone Array.
+
+ */
 function getRandomQuote() {
 
   if (numberOfQuotes > 0) {
@@ -173,21 +206,19 @@ function getRandomQuote() {
     var randomQuote = quotes[randomIndex];
     quotesClone.push(randomQuote);
 
-    if (currentQuote.length === 0) {
-      currentQuote.push(randomQuote);
-
-    } else {
-      currentQuote = [];
-      currentQuote.push(randomQuote);
-
-    }
+    // if (currentQuote.length === 0) {
+    //   currentQuote.push(randomQuote);
+    //
+    // } else {
+    //   currentQuote = [];
+    //   currentQuote.push(randomQuote);
+    //
+    // }
 
     test(quotes[randomIndex]);
     return quotes[randomIndex];
 
-  }
-
-  if (numberOfQuotes === 0) {
+  } else {
     quotes = quotesClone;
     quotesClone = [];
     randomIndex = (Math.floor(Math.random() * numberOfQuotes));
@@ -239,8 +270,8 @@ function printQuote() {
 
   //  Capture Profile's Quote Content
   var copy = "";
-  copy += "<p class=\"author\">" + profile.author + "</p>";
-  copy += "<p class=\"ids\">" + profile.ids + "</p>";
+  copy += "<p class=\"author\">" + profile.source + "</p>";
+  copy += "<p class=\"ids\">" + profile.tags + "</p>";
   copy += "<div class=\"separator\"></div>";
   copy += "<p class=\"quote\">" + profile.quote + "</p>";
 
