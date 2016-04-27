ffc140ec0361cf5813f96c099b48adb67ca864c9
Claudio Vallejo <hola@claudiovallejo.mx>
2016-04-26 20:55:04 -0500

    Restructured respository

--- Project 1/Assets/Downloadable/js/script.js
+++
@@ -1,4 +0,0 @@
-// event listener to respond to clicks on the page
-// when user clicks anywhere on the page, the "makeQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
